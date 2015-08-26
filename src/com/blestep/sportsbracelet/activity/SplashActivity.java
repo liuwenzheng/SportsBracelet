12987292a7fc7166445a8929a0d6e302c5a53fd5
刘文政 <wenzheng.liu@corp.elong.com>
2015-08-26 18:50:04 +0800

    环形view

--- src/com/blestep/sportsbracelet/activity/SplashActivity.java
+++ src/com/blestep/sportsbracelet/activity/SplashActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 
 import com.blestep.sportsbracelet.R;
 import com.blestep.sportsbracelet.base.BaseActivity;
+import com.blestep.sportsbracelet.view.CircleProgressView;
 
 public class SplashActivity extends BaseActivity {
+
+	private CircleProgressView circleView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.splash);
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		circleView = (CircleProgressView) findViewById(R.id.circleView);
+		circleView.setMaxValue(100);
+		circleView.setValueAnimated(35);
 	}
 }
