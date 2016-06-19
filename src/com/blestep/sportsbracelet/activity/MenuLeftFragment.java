fb871e59661d7b36fe9693259190a33c15fdec1a
329541594 <329541594@qq.com>
2016-06-19 23:30:28 +0800

    “单位转换”

--- src/com/blestep/sportsbracelet/activity/MenuLeftFragment.java
+++ src/com/blestep/sportsbracelet/activity/MenuLeftFragment.java
@@ -1,5 +1,6 @@
 package com.blestep.sportsbracelet.activity;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
@@ -24,193 +25,186 @@ import com.blestep.sportsbracelet.utils.SPUtiles;
 import com.blestep.sportsbracelet.utils.ToastUtils;
 
 public class MenuLeftFragment extends Fragment implements OnClickListener {
-	private View mView;
-	private MainActivity mainActivity;
-	private TextView tv_bracelet_name, tv_alert_coming_call_state;
-	private ImageView iv_battery_state, iv_conn_state;
-	private CheckBox cb_alert_low_battery, cb_alert_find_band;
-	private BTService mBtService;
-	private boolean isContinue = false;
+    private View mView;
+    private MainActivity mainActivity;
+    private TextView tv_bracelet_name, tv_alert_coming_call_state;
+    private ImageView iv_battery_state, iv_conn_state;
+    private CheckBox cb_alert_low_battery, cb_alert_find_band;
+    private BTService mBtService;
+    private boolean isContinue = false;
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		mainActivity = (MainActivity) getActivity();
-		super.onActivityCreated(savedInstanceState);
-	}
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        mainActivity = (MainActivity) getActivity();
+        super.onActivityCreated(savedInstanceState);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		if (mView == null) {
-			mView = inflater.inflate(R.layout.left_menu, container, false);
-		}
-		initView();
-		initListener();
-		initData();
-		return mView;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (mView == null) {
+            mView = inflater.inflate(R.layout.left_menu, container, false);
+        }
+        initView();
+        initListener();
+        initData();
+        return mView;
+    }
 
-	@Override
-	public void onResume() {
-		initData();
-		super.onResume();
-	}
+    @Override
+    public void onResume() {
+        initData();
+        super.onResume();
+    }
 
-	private void initView() {
-		tv_bracelet_name = (TextView) mView.findViewById(R.id.tv_bracelet_name);
-		tv_alert_coming_call_state = (TextView) mView
-				.findViewById(R.id.tv_alert_coming_call_state);
-		iv_battery_state = (ImageView) mView
-				.findViewById(R.id.iv_battery_state);
-		iv_conn_state = (ImageView) mView.findViewById(R.id.iv_conn_state);
-		cb_alert_low_battery = (CheckBox) mView
-				.findViewById(R.id.cb_alert_low_battery);
-		cb_alert_find_band = (CheckBox) mView
-				.findViewById(R.id.cb_alert_find_band);
-		// if (mainActivity.getmBtService() != null &&
-		// mainActivity.getmBtService().isConnDevice()) {
-		// cb_alert_find_band.setEnabled(true);
-		// } else {
-		// cb_alert_find_band.setEnabled(false);
-		// }
-		cb_alert_find_band
-				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+    private void initView() {
+        tv_bracelet_name = (TextView) mView.findViewById(R.id.tv_bracelet_name);
+        tv_alert_coming_call_state = (TextView) mView
+                .findViewById(R.id.tv_alert_coming_call_state);
+        iv_battery_state = (ImageView) mView
+                .findViewById(R.id.iv_battery_state);
+        iv_conn_state = (ImageView) mView.findViewById(R.id.iv_conn_state);
+        cb_alert_low_battery = (CheckBox) mView
+                .findViewById(R.id.cb_alert_low_battery);
+        cb_alert_find_band = (CheckBox) mView
+                .findViewById(R.id.cb_alert_find_band);
+        // if (mainActivity.getmBtService() != null &&
+        // mainActivity.getmBtService().isConnDevice()) {
+        // cb_alert_find_band.setEnabled(true);
+        // } else {
+        // cb_alert_find_band.setEnabled(false);
+        // }
+        cb_alert_find_band
+                .setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-					@Override
-					public void onCheckedChanged(CompoundButton buttonView,
-							boolean isChecked) {
-						if (isChecked) {
-							if (mainActivity.getmBtService() != null) {
-								mBtService = mainActivity.getmBtService();
-								if (mBtService.isConnDevice()) {
-									isContinue = true;
-									startFindBandShake();
-								} else {
-									ToastUtils
-											.showToast(
-													mainActivity,
-													R.string.alert_find_band_conn_false);
-									cb_alert_find_band.setChecked(false);
-								}
-							} else {
-								ToastUtils.showToast(mainActivity,
-										R.string.alert_find_band_conn_false);
-								cb_alert_find_band.setChecked(false);
-							}
-						} else {
-							isContinue = false;
-						}
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView,
+                                                 boolean isChecked) {
+                        if (isChecked) {
+                            if (mainActivity.getmBtService() != null) {
+                                mBtService = mainActivity.getmBtService();
+                                if (mBtService.isConnDevice()) {
+                                    isContinue = true;
+                                    startFindBandShake();
+                                } else {
+                                    ToastUtils
+                                            .showToast(
+                                                    mainActivity,
+                                                    R.string.alert_find_band_conn_false);
+                                    cb_alert_find_band.setChecked(false);
+                                }
+                            } else {
+                                ToastUtils.showToast(mainActivity,
+                                        R.string.alert_find_band_conn_false);
+                                cb_alert_find_band.setChecked(false);
+                            }
+                        } else {
+                            isContinue = false;
+                        }
 
-					}
-				});
-	}
+                    }
+                });
+    }
 
-	private void startFindBandShake() {
-		new Thread() {
-			public void run() {
-				while (isContinue) {
-					mBtService.shakeFindBand();
-					try {
-						Thread.sleep(4000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			};
-		}.start();
-	}
+    private void startFindBandShake() {
+        new Thread() {
+            public void run() {
+                while (isContinue) {
+                    mBtService.shakeFindBand();
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
-	private void initListener() {
-		mView.findViewById(R.id.rl_alert_coming_call).setOnClickListener(this);
-		mView.findViewById(R.id.rl_alert_alarm).setOnClickListener(this);
-		mView.findViewById(R.id.rl_bind_bracelet).setOnClickListener(this);
-		mView.findViewById(R.id.rl_bracelet_reset).setOnClickListener(this);
-		mView.findViewById(R.id.rl_about).setOnClickListener(this);
-	}
+            ;
+        }.start();
+    }
 
-	private void initData() {
-		tv_bracelet_name.setText(SPUtiles.getStringValue(
-				BTConstants.SP_KEY_DEVICE_NAME, ""));
-		if (SPUtiles.getBooleanValue(BTConstants.SP_KEY_COMING_PHONE_ALERT,
-				true)) {
-			tv_alert_coming_call_state
-					.setText(getString(R.string.alert_coming_call_open));
-		} else {
-			tv_alert_coming_call_state
-					.setText(getString(R.string.alert_coming_call_close));
-		}
-	}
+    private void initListener() {
+        mView.findViewById(R.id.rl_alert_coming_call).setOnClickListener(this);
+        mView.findViewById(R.id.rl_alert_alarm).setOnClickListener(this);
+        mView.findViewById(R.id.rl_bind_bracelet).setOnClickListener(this);
+        mView.findViewById(R.id.rl_bracelet_system).setOnClickListener(this);
+        mView.findViewById(R.id.rl_about).setOnClickListener(this);
+    }
 
-	public void updateView(BTService mBtService) {
-		if (mBtService.isConnDevice()) {
-			iv_conn_state.setImageResource(R.drawable.conn_state_success);
-		} else {
-			iv_conn_state.setImageResource(R.drawable.conn_state_failure);
-		}
-		int battery = SPUtiles.getIntValue(BTConstants.SP_KEY_BATTERY, 0);
-		if (battery == 0) {
-			iv_battery_state.setImageResource(R.drawable.battery_one);
-		}
-		if (battery == 25) {
-			iv_battery_state.setImageResource(R.drawable.battery_one);
-		}
-		if (battery == 50) {
-			iv_battery_state.setImageResource(R.drawable.battery_two);
-		}
-		if (battery == 75) {
-			iv_battery_state.setImageResource(R.drawable.battery_three);
-		}
-		if (battery == 100) {
-			iv_battery_state.setImageResource(R.drawable.battery_four);
-		}
-	}
+    private void initData() {
+        tv_bracelet_name.setText(SPUtiles.getStringValue(
+                BTConstants.SP_KEY_DEVICE_NAME, ""));
+        if (SPUtiles.getBooleanValue(BTConstants.SP_KEY_COMING_PHONE_ALERT,
+                true)) {
+            tv_alert_coming_call_state
+                    .setText(getString(R.string.alert_coming_call_open));
+        } else {
+            tv_alert_coming_call_state
+                    .setText(getString(R.string.alert_coming_call_close));
+        }
+    }
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.rl_alert_coming_call:
-			startActivity(new Intent(mainActivity, PhoneComingActivity.class));
-			break;
-		case R.id.rl_alert_alarm:
-			startActivity(new Intent(mainActivity, AlarmActivity.class));
-			break;
-		case R.id.rl_bind_bracelet:
-			startActivity(new Intent(mainActivity, BindDeviceActivity.class));
-			break;
-		case R.id.rl_bracelet_reset:
-			AlertDialog.Builder builder = new Builder(mainActivity);
-			builder.setMessage(R.string.bracelet_reset_alert);
-			builder.setPositiveButton(R.string.bracelet_reset_alert_confirm,
-					new DialogInterface.OnClickListener() {
+    public void updateView(BTService mBtService) {
+        if (mBtService.isConnDevice()) {
+            iv_conn_state.setImageResource(R.drawable.conn_state_success);
+        } else {
+            iv_conn_state.setImageResource(R.drawable.conn_state_failure);
+        }
+        int battery = SPUtiles.getIntValue(BTConstants.SP_KEY_BATTERY, 0);
+        if (battery == 0) {
+            iv_battery_state.setImageResource(R.drawable.battery_one);
+        }
+        if (battery == 25) {
+            iv_battery_state.setImageResource(R.drawable.battery_one);
+        }
+        if (battery == 50) {
+            iv_battery_state.setImageResource(R.drawable.battery_two);
+        }
+        if (battery == 75) {
+            iv_battery_state.setImageResource(R.drawable.battery_three);
+        }
+        if (battery == 100) {
+            iv_battery_state.setImageResource(R.drawable.battery_four);
+        }
+    }
 
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							SPUtiles.clearAllData();
-							DBTools.getInstance(mainActivity).deleteAllData();
-							mainActivity.getmBtService().mBluetoothGatt.close();
-							mainActivity.getmBtService().mBluetoothGatt = null;
-							dialog.dismiss();
-							mainActivity.finish();
-						}
-					});
-			builder.setNegativeButton(R.string.bracelet_reset_alert_cancel,
-					new DialogInterface.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rl_alert_coming_call:
+                startActivity(new Intent(mainActivity, PhoneComingActivity.class));
+                break;
+            case R.id.rl_alert_alarm:
+                startActivity(new Intent(mainActivity, AlarmActivity.class));
+                break;
+            case R.id.rl_bind_bracelet:
+                startActivity(new Intent(mainActivity, BindDeviceActivity.class));
+                break;
+            case R.id.rl_bracelet_system:
+                Intent intent = new Intent(mainActivity, SystemActivity.class);
+                startActivityForResult(intent, BTConstants.REQUEST_CODE_SYSTEM);
+                break;
+            case R.id.rl_about:
+                startActivity(new Intent(mainActivity, AboutActivity.class));
+                break;
 
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							dialog.dismiss();
-						}
-					});
-			builder.show();
-			break;
-		case R.id.rl_about:
-			startActivity(new Intent(mainActivity, AboutActivity.class));
-			break;
+            default:
+                break;
+        }
 
-		default:
-			break;
-		}
+    }
 
-	}
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == BTConstants.REQUEST_CODE_SYSTEM) {
+                SPUtiles.clearAllData();
+                DBTools.getInstance(mainActivity).deleteAllData();
+                mainActivity.getmBtService().mBluetoothGatt.close();
+                mainActivity.getmBtService().mBluetoothGatt = null;
+                mainActivity.finish();
+            }
+        }
+    }
 }
--- src/com/blestep/sportsbracelet/activity/SettingUserInfoActivity.java
+++ src/com/blestep/sportsbracelet/activity/SettingUserInfoActivity.java
@@ -1,197 +1,151 @@
 package com.blestep.sportsbracelet.activity;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
 import com.blestep.sportsbracelet.BTConstants;
 import com.blestep.sportsbracelet.R;
 import com.blestep.sportsbracelet.base.BaseActivity;
+import com.blestep.sportsbracelet.module.UnitManagerModule;
 import com.blestep.sportsbracelet.utils.SPUtiles;
 import com.blestep.sportsbracelet.utils.ToastUtils;
 import com.blestep.sportsbracelet.utils.Utils;
 import com.umeng.analytics.MobclickAgent;
 
-public class SettingUserInfoActivity extends BaseActivity implements
-		OnClickListener, OnCheckedChangeListener {
-	private EditText et_setting_userinfo_name, et_setting_userinfo_height,
-			et_setting_userinfo_weight;
-	private ImageView iv_setting_userinfo_icon;
-	private RadioGroup rg_setting_userinfo_sex;
-	private TextView tv_setting_userinfo_birthday;
-	private Button btn_setting_next;
-	private DatePickerDialog mDialog;
-	private Calendar mCalendar;
-	private SimpleDateFormat sdf = new SimpleDateFormat(
-			BTConstants.PATTERN_YYYY_MM_DD);
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.setting_userinfo);
-		initView();
-		initListener();
-		initData();
-	}
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
-	private void initListener() {
-		rg_setting_userinfo_sex.setOnCheckedChangeListener(this);
-		tv_setting_userinfo_birthday.setOnClickListener(this);
-		btn_setting_next.setOnClickListener(this);
-	}
+public class SettingUserInfoActivity extends BaseActivity {
+    @InjectView(R.id.et_setting_userinfo_name)
+    EditText et_setting_userinfo_name;
+    @InjectView(R.id.rg_setting_userinfo_sex)
+    RadioGroup rg_setting_userinfo_sex;
+    @InjectView(R.id.tv_setting_userinfo_birthday)
+    TextView tv_setting_userinfo_birthday;
+    @InjectView(R.id.tv_setting_userinfo_height)
+    TextView tv_setting_userinfo_height;
+    @InjectView(R.id.tv_setting_userinfo_weight)
+    TextView tv_setting_userinfo_weight;
+    private DatePickerDialog mDialog;
+    private Calendar mCalendar;
+    private SimpleDateFormat sdf = new SimpleDateFormat(
+            BTConstants.PATTERN_YYYY_MM_DD);
+    private UnitManagerModule module;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_userinfo);
+        ButterKnife.inject(this);
+        initData();
+    }
 
-	private void initData() {
-		((RadioButton) rg_setting_userinfo_sex.getChildAt(0)).setChecked(true);
-		mCalendar = Calendar.getInstance();
-		try {
-			Date date = sdf.parse(tv_setting_userinfo_birthday.getText()
-					.toString());
-			mCalendar.setTime(date);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		mDialog = new DatePickerDialog(this, R.style.AppTheme_Dialog,
-				new OnDateSetListener() {
+    private void initData() {
+        ((RadioButton) rg_setting_userinfo_sex.getChildAt(0)).setChecked(true);
+        mCalendar = Calendar.getInstance();
+        try {
+            Date date = sdf.parse(tv_setting_userinfo_birthday.getText()
+                    .toString());
+            mCalendar.setTime(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        mDialog = new DatePickerDialog(this, R.style.AppTheme_Dialog,
+                new OnDateSetListener() {
 
-					@Override
-					public void onDateSet(DatePicker view, int year,
-							int monthOfYear, int dayOfMonth) {
-						mCalendar.set(Calendar.YEAR, year);
-						mCalendar.set(Calendar.MONTH, monthOfYear);
-						mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-						tv_setting_userinfo_birthday.setText(sdf
-								.format(mCalendar.getTime()));
-					}
-				}, mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH),
-				mCalendar.get(Calendar.DAY_OF_MONTH));
-	}
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        mCalendar.set(Calendar.YEAR, year);
+                        mCalendar.set(Calendar.MONTH, monthOfYear);
+                        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        tv_setting_userinfo_birthday.setText(sdf
+                                .format(mCalendar.getTime()));
+                    }
+                }, mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH),
+                mCalendar.get(Calendar.DAY_OF_MONTH));
+        module = new UnitManagerModule();
+    }
 
-	private void initView() {
-		et_setting_userinfo_name = (EditText) findViewById(R.id.et_setting_userinfo_name);
-		et_setting_userinfo_height = (EditText) findViewById(R.id.et_setting_userinfo_height);
-		et_setting_userinfo_weight = (EditText) findViewById(R.id.et_setting_userinfo_weight);
-		rg_setting_userinfo_sex = (RadioGroup) findViewById(R.id.rg_setting_userinfo_sex);
-		tv_setting_userinfo_birthday = (TextView) findViewById(R.id.tv_setting_userinfo_birthday);
-		btn_setting_next = (Button) findViewById(R.id.btn_setting_next);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.tv_setting_userinfo_birthday:
-			mDialog.show();
-			break;
-		case R.id.btn_setting_next:
-			if (Utils.isEmpty(et_setting_userinfo_name.getText().toString())) {
-				ToastUtils.showToast(this,
-						R.string.setting_userinfo_name_not_null);
-				return;
-			}
-			if (Utils.isEmpty(et_setting_userinfo_height.getText().toString())) {
-				ToastUtils.showToast(this,
-						R.string.setting_userinfo_height_not_null);
-				return;
-			}
-			if (Integer
-					.valueOf(et_setting_userinfo_height.getText().toString()) < 100
-					|| Integer.valueOf(et_setting_userinfo_height.getText()
-							.toString()) > 200) {
-				ToastUtils.showToast(this,
-						R.string.setting_userinfo_height_size);
-				return;
-			}
-			if (Utils.isEmpty(et_setting_userinfo_weight.getText().toString())) {
-				ToastUtils.showToast(this,
-						R.string.setting_userinfo_weight_not_null);
-				return;
-			}
-			if (Integer
-					.valueOf(et_setting_userinfo_weight.getText().toString()) < 30
-					|| Integer.valueOf(et_setting_userinfo_weight.getText()
-							.toString()) > 150) {
-				ToastUtils.showToast(this,
-						R.string.setting_userinfo_weight_size);
-				return;
-			}
-			// 计算年龄
-			Calendar current = Calendar.getInstance();
-			try {
-				Date birthday = sdf.parse(tv_setting_userinfo_birthday
-						.getText().toString());
-				Calendar birthdayCalendar = Calendar.getInstance();
-				birthdayCalendar.setTime(birthday);
-				int age = current.get(Calendar.YEAR)
-						- birthdayCalendar.get(Calendar.YEAR);
-				if (age < 5 || age > 99) {
-					ToastUtils.showToast(this,
-							R.string.setting_userinfo_age_size);
-					return;
-				}
-				SPUtiles.setIntValue(BTConstants.SP_KEY_USER_AGE, age);
-				SPUtiles.setStringValue(BTConstants.SP_KEY_USER_BIRTHDAT,
-						tv_setting_userinfo_birthday.getText().toString());
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
 
-			SPUtiles.setStringValue(BTConstants.SP_KEY_USER_NAME,
-					et_setting_userinfo_name.getText().toString());
-			SPUtiles.setIntValue(BTConstants.SP_KEY_USER_HEIGHT, Integer
-					.valueOf(et_setting_userinfo_height.getText().toString()));
-			SPUtiles.setIntValue(BTConstants.SP_KEY_USER_WEIGHT, Integer
-					.valueOf(et_setting_userinfo_weight.getText().toString()));
+    @OnClick({R.id.tv_setting_userinfo_height, R.id.tv_setting_userinfo_weight, R.id.tv_setting_userinfo_birthday, R.id.btn_setting_next})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_setting_userinfo_height:
+                module.createHeightDialog(this);
+                break;
+            case R.id.tv_setting_userinfo_weight:
+                module.createWeightDialog(this);
+                break;
+            case R.id.tv_setting_userinfo_birthday:
+                mDialog.show();
+                break;
+            case R.id.btn_setting_next:
+                if (Utils.isEmpty(et_setting_userinfo_name.getText().toString())) {
+                    ToastUtils.showToast(this,
+                            R.string.setting_userinfo_name_not_null);
+                    return;
+                }
+                // 计算年龄
+                Calendar current = Calendar.getInstance();
+                try {
+                    Date birthday = sdf.parse(tv_setting_userinfo_birthday
+                            .getText().toString());
+                    Calendar birthdayCalendar = Calendar.getInstance();
+                    birthdayCalendar.setTime(birthday);
+                    int age = current.get(Calendar.YEAR)
+                            - birthdayCalendar.get(Calendar.YEAR);
+                    if (age < 5 || age > 99) {
+                        ToastUtils.showToast(this,
+                                R.string.setting_userinfo_age_size);
+                        return;
+                    }
+                    SPUtiles.setIntValue(BTConstants.SP_KEY_USER_AGE, age);
+                    SPUtiles.setStringValue(BTConstants.SP_KEY_USER_BIRTHDAT,
+                            tv_setting_userinfo_birthday.getText().toString());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
 
-			SPUtiles.setIntValue(BTConstants.SP_KEY_USER_GENDER, Integer
-					.valueOf((String) findViewById(
-							rg_setting_userinfo_sex.getCheckedRadioButtonId())
-							.getTag()));
-			startActivity(new Intent(this, SettingTargetActivity.class));
-			this.finish();
-			break;
+                SPUtiles.setStringValue(BTConstants.SP_KEY_USER_NAME,
+                        et_setting_userinfo_name.getText().toString());
+                SPUtiles.setIntValue(BTConstants.SP_KEY_USER_HEIGHT, Integer
+                        .valueOf(tv_setting_userinfo_height.getTag().toString()));
+                SPUtiles.setIntValue(BTConstants.SP_KEY_USER_WEIGHT, Integer
+                        .valueOf(tv_setting_userinfo_weight.getTag().toString()));
 
-		default:
-			break;
-		}
-
-	}
-
-	@Override
-	public void onCheckedChanged(RadioGroup group, int checkedId) {
-		switch (checkedId) {
-		case R.id.rb_setting_userinfo_male:
-			break;
-		case R.id.rb_setting_userinfo_female:
-			break;
-
-		default:
-			break;
-		}
-	}
-	@Override
-	protected void onResume() {
-		super.onResume();
-		MobclickAgent.onResume(this);
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-		MobclickAgent.onPause(this);
-	}
+                SPUtiles.setIntValue(BTConstants.SP_KEY_USER_GENDER, Integer
+                        .valueOf((String) findViewById(
+                                rg_setting_userinfo_sex.getCheckedRadioButtonId())
+                                .getTag()));
+                startActivity(new Intent(this, SettingTargetActivity.class));
+                this.finish();
+                break;
+        }
+    }
 }
