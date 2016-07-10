071634b6483757847615f592d2d72e9e61e043a6
329541594 <329541594@qq.com>
2016-07-11 00:03:54 +0800

    新导航页

---
+++ bracelet/src/com/blestep/sportsbracelet/activity/GuideActivity.java
@@ -0,0 +1,15 @@
+package com.blestep.sportsbracelet.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.blestep.sportsbracelet.R;
+
+public class GuideActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
